c780f40f2a3e8aaebbdcd9e3d0f0544dff296faa
myura <MisterYura@gmail.com>
2017-06-27 09:35:06 -0400

    full implementation of frequency to horizontal bar height, genericization of RtxPlayer as interface (with Monitor and Screen subclasses)

--- js/midimonitor.js
+++ js/midimonitor.js
@@ -1,66 +1,21 @@
 class MidiMonitor {
+	static freq2Px(frequency, screenHeight = window.screen.height, fps = 60) {
+		var minFrequency = fps;
+		var maxFrequency = minFrequency * screenHeight / 2;
+		return frequency > 0 ? maxFrequency / frequency : 0;
+	}
+
 	constructor(canvas) {
 		this.canvas = canvas;
-		this.tempVar = false;
 	}
 
-	doit() {
-		if (this.tempVar) {
-			note('30px', 0.8)
-		}
-		else {
-			note('50px', 0.3)
-		}
-		this.tempVar = !this.tempVar;
+	setFrequency(frequency) {
+		var size = MidiMonitor.freq2Px(frequency);
+		this.canvas.style.backgroundSize = '100% ' + size + 'px';
 	}
 
-	setPitch() {
-
+	setVelocity(velocity) {
+		var opacity = velocity;
+		this.canvas.opacity = opacity;
 	}
 }
-
-
-function midiMonitorElement() {
-	var elementId = 'midimonitor';
-	return document.getElementById(elementId)
-}
-
-function midiMonitorStyle() {
-	var elementId = 'midimonitor';
-	return document.getElementById(elementId).style
-}
-
-function screenHeight() {
-	return window.screen.height
-}
-
-
-function duration(time) {
-	midiMonitorStyle().transitionDuration = time;
-}
-
-function pitch(size) {
-	midiMonitorStyle().backgroundSize = "100% " + size;
-}
-
-function velocity(opacity) {
-	midiMonitorStyle().opacity = opacity;
-}
-
-
-function heightStyleString(pitch) {
-	var size = pitch;
-	return `background-size: 100% ${size}px;`
-}
-function opacityStyleString(opacity) {
-	return `opacity: ${opacity};`
-}
-
-
-
-function note(pitch, velocity = 100) {
-
-	midiMonitorElement().setAttribute('style', heightStyleString(pitch) + opacityStyleString(velocity));
-}
-
-
--- js/rtxplayer.js
+++ js/rtxplayer.js
@@ -1,5 +1,22 @@
 class RtxPlayer {
+	setGain(gain = 1) {
+		this._setGain(Math.max(Math.min(gain, 1), 0));
+	}
+	_setGain(gain) {
+		console.warn("TO IMPLEMENT: " + "_setGain");
+	}
+
+	playTones(rtx) {
+		return(this._playTones(rtx));
+	}
+	_playTones(rtx) {
+		console.warn("TO IMPLEMENT: " + "_playTones");
+	}
+}
+
+class RtxSoundPlayer extends RtxPlayer {
 	constructor(midiMonitor) {
+		super();
 		this.audioContext = new window.AudioContext();
 
 		this.gainNode = this.audioContext.createGain();
@@ -7,11 +24,11 @@ class RtxPlayer {
 		this.setGain();
 	}
 
-	setGain(gain = 0.01) {
-		this.gainNode.gain.value = gain;
+	_setGain(gain) {
+		this.gainNode.gain.value = gain * 0.1;
 	}
 
-	playTonesORIG(rtx) {
+	_playTones(rtx) {
 		if (this.isPlaying) {
 			this.oscillatorNode.stop();
 			this.isPlaying = false;
@@ -34,15 +51,30 @@ class RtxPlayer {
 			this.oscillatorNode.stop(relativeTime);
 			this.isPlaying = true;
 		}
+		return(this.isPlaying);
+	}
+}
 
-		return this.isPlaying;
+class RtxMonitorPlayer extends RtxPlayer {
+	constructor(midiMonitor) {
+		super();
+		this.midiMonitor = midiMonitor;
 	}
 
-	playTones(rtx) {
+	_setGain(gain) {
+		this.midiMonitor.setVelocity(gain);
+	}
+
+	_playTones(rtx) {
 		var totalTimeout = 0;
 		for (let rtxLoop = 0; rtxLoop <= rtx.controls.LOOPS; rtxLoop++) {
 			for (let rtxTone of rtx.tones) {
-				setTimeout(note, totalTimeout, rtxTone.noteFrequency);
+				setTimeout(function(frequency) {
+					this.midiMonitor.setFrequency(frequency);
+				}, totalTimeout, rtxTone.noteFrequency);
+				setTimeout(function(frequency) {
+					this.midiMonitor.setFrequency(frequency);
+				}, totalTimeout + (rtxTone.durationStyleTime * 1000), 0);
 				totalTimeout += rtxTone.durationTime * 1000;
 			}
 		}
